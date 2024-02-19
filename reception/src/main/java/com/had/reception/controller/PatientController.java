6a6e16837cfa49224b9e0c96b41c2ce7c78cdd2b
rishi-vakharia <rishi.rvv@gmail.com>
2024-02-19 17:11:20 +0530

    added fix -couldn't add more than 1 patient

--- reception/src/main/java/com/had/reception/controller/PatientController.java
+++ reception/src/main/java/com/had/reception/controller/PatientController.java
@@ -2,6 +2,7 @@ package com.had.reception.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import com.had.reception.models.Patient;
 import com.had.reception.service.PatientService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/patient")
 public class PatientController {
     
--- reception/src/main/java/com/had/reception/models/Patient.java
+++ reception/src/main/java/com/had/reception/models/Patient.java
@@ -1,6 +1,8 @@
 package com.had.reception.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,12 +16,13 @@ import lombok.NoArgsConstructor;
 public class Patient {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String name;
     private int age;
     private String bloodGroup;
     private String gender;
-    private String contact;
+    private String phoneNo;
     private String address;
     
 }
