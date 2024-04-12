6acef8f0df9b5ca5f5976e9df0375f942ffd364b
Amar-Pratap-Singh <AmarPratap.Singh@iiitb.ac.in>
2024-04-12 15:44:07 +0530

    Waste commit

--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -63,14 +63,14 @@ function SignIn() {
   return (
     <IonPage>
       <div className='p-3 max-w-4xl mx-auto'>
-        <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
-        <form className='flex flex-col' onSubmit={handleSubmit(onSubmit)}>
-          <TextInput name='email' placeHolder='Enter email' label='Email' control={control}/>
-          <TextInput name='password' placeHolder='Enter password' label='Password' control={control}/>
-          <IonButton type='submit'>Sign In</IonButton>
-        </form>    
-        <p className='text-red-700 mt-5'>{error && 'Something went wrong!'}</p>
-      </div>
+          <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+          <form className='flex flex-col' onSubmit={handleSubmit(onSubmit)}>
+            <TextInput name='email' placeHolder='Enter email' label='Email' control={control}/>
+            <TextInput name='password' placeHolder='Enter password' label='Password' control={control}/>
+            <IonButton type='submit'>Sign In</IonButton>
+          </form>    
+          <p className='text-red-700 mt-5'>{error && 'Something went wrong!'}</p>
+        </div>
     </IonPage>
   )
 }
