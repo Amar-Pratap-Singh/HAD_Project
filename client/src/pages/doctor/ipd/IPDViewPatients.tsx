0f78826f56fc25563ea1a1af359fc3490a39970f
rishi-vakharia <rishi.rvv@gmail.com>
2024-04-30 00:57:24 +0530

    fixed doctor side ui

--- client/src/pages/doctor/ipd/IPDViewPatients.tsx
+++ client/src/pages/doctor/ipd/IPDViewPatients.tsx
@@ -2,9 +2,9 @@ import React, { useState, useEffect } from "react";
 import { IonPage, IonContent, IonCol, IonGrid, IonRow, IonButton } from "@ionic/react";
 import { useHistory } from 'react-router-dom';
 import Header from "../../../components/Header";
-import "./IPDViewPatients.css";
 
 const IPDViewPatients: React.FC = () => {
+
   const [patients, setPatients] = useState<any[]>([]);
   const history = useHistory();
   
@@ -14,7 +14,6 @@ const IPDViewPatients: React.FC = () => {
 
   const fetchData = async () => {
     try {
-        // fetching data from reception service
       const response = await fetch(
         "http://localhost:8081/patient/get-ipd-appointments"
       );
@@ -23,55 +22,56 @@ const IPDViewPatients: React.FC = () => {
       }
       const data = await response.json();
       setPatients(data);
-    } catch (error) {
+    } 
+    catch (error) {
       console.error("Error fetching data:", error);
     }
   };
 
   const viewPatientDetails = (patientId: any) => {
-    // Navigate to the patient details page with the patient ID
+    //navigate to the patient details page
     history.push(`/doctor/ipd/patient-details/` + patientId);
     location.reload()
   };
 
   const addEncounter = (patientId: any) => {
-    // Navigate to the add encounter page
+    //navigate to the add encounter page
     history.push(`/doctor/ipd/add-encounter/`+ patientId);
   };
 
   return (
-    <div className="view-patients">
-      <IonPage>
-        <Header />
-        <IonContent>
-          <h1>Patients</h1>
-          <IonGrid className="table">
-            <IonRow className="table-header">
-              <IonCol>Patient ID</IonCol>
-              <IonCol>Room No</IonCol>
-              <IonCol>Ward No</IonCol>
-              <IonCol></IonCol>
-              <IonCol></IonCol> {/* Add an empty column for spacing */}
-            </IonRow>
-            {patients.map((patient) => (
-              <IonRow key={patient.id}>
-                <IonCol>{patient.patientId}</IonCol>
-                <IonCol>{patient.bed_no}</IonCol>
-                <IonCol>{patient.ward_no}</IonCol>
-                <IonCol>
-                  {/* Button to view patient details */}
-                  <IonButton onClick={() => viewPatientDetails(patient.patientId)}>View Details</IonButton>
-                </IonCol>
-                <IonCol>
-                  {/* Button to add encounter */}
-                  <IonButton onClick={() => addEncounter(patient.patientId)}>Add Encounter</IonButton>
-                </IonCol>
+    <IonPage>
+      <Header />
+      <IonContent>
+        <h1 className='text-center text-xl font-semibold my-5'>IPD Patients</h1>
+        <IonGrid className="border-2 border-solid border-black mx-20 mb-5">
+          <IonRow className="border-b-2 border-solid border-black font-semibold">
+            <IonCol size="1">Patient ID</IonCol>
+            <IonCol size="1">Ward No</IonCol>
+            <IonCol size="1">Bed No</IonCol>
+            <IonCol>Dept</IonCol>
+            <IonCol>Doctor</IonCol>
+            <IonCol>Details</IonCol>
+            <IonCol>Add Encounter</IonCol>
+            <IonCol>Discharge</IonCol>
+          </IonRow>
+          {
+            patients.map(patient => (
+              <IonRow key={patient.patientId}>
+                <IonCol size="1">{patient.patientId}</IonCol>
+                <IonCol size="1">{patient.wardNo}</IonCol>
+                <IonCol size="1">{patient.bedNo}</IonCol>
+                <IonCol>{patient.department}</IonCol>
+                <IonCol>{patient.doctorName}</IonCol>
+                <IonCol><IonButton size="small" onClick={() => viewPatientDetails(patient.patientId)}>View</IonButton></IonCol>
+                <IonCol><IonButton size="small" onClick={() => addEncounter(patient.patientId)}>Add</IonButton></IonCol>
+                <IonCol><IonButton size="small" onClick={() => console.log('discharge')}>Discharge</IonButton></IonCol>
               </IonRow>
-            ))}
-          </IonGrid>
-        </IonContent>
-      </IonPage>
-    </div>
+            ))
+          }
+        </IonGrid>
+      </IonContent>
+    </IonPage>
   );
 };
 
--- client/src/pages/doctor/ipd/PatientDetails.tsx
+++ client/src/pages/doctor/ipd/PatientDetails.tsx
@@ -5,17 +5,20 @@ import {
   IonCardHeader,
   IonCardSubtitle,
   IonCardTitle,
+  IonContent,
+  IonPage,
 } from "@ionic/react";
-import Header from "../../../components/Header";
 import { useParams } from "react-router";
+import Header from "../../../components/Header";
 import DrawingImage from "../../../toolkit/DrawingImage";
 import AudioPlayback from "../../../toolkit/AudioPlayback";
 
 interface PatientDetails {
+  id: number;
   name: string;
-  patientId: string;
   age: number;
-  reasonForAdmit: string;
+  bloodGroup: string;
+  gender: string;
 }
 
 interface Encounter {
@@ -24,20 +27,41 @@ interface Encounter {
   prescriptionId: number;
 }
 
+interface Prescription {
+  prescriptionId: number;
+  notes: string;
+  instructions: string;
+  scribbleNotes: string;
+  scribbleInstructions: string;
+  audioNotes: string;
+  audioInstructions: string;
+}
 
-const PatientDetails: React.FC = () => {
+interface Medication {
+  medicationId: number;
+  prescriptionId: number;
+  medicineName: string;
+  quantity: number;
+  time: string;
+  duration: string;
+}
 
-  const { patientId } = useParams<{ patientId: string }>();
-  console.log(patientId);
-  const [patientDetails, setPatientDetails] = useState<PatientDetails | null>(null);
-  const [encounters, setEncounters] = useState<Encounter[]>([]);
+interface EncounterWrapper {
+  encounter: Encounter;
+  prescription: Prescription;
+  medications: Medication[];
+}
+
+const PatientDetailsPage: React.FC = () => {
+
+  const { patientId } = useParams<{ patientId: string }>();;
+  const [patientDetails, setPatientDetails] = useState<PatientDetails>();
+  const [encounters, setEncounters] = useState<EncounterWrapper[]>([]);
 
   useEffect(() => {
     fetchPatientDetails();
     fetchEncounters();
   }, []);
-
-
 
   const fetchPatientDetails = async () => {
     try {
@@ -47,131 +71,139 @@ const PatientDetails: React.FC = () => {
       }
       const data = await response.json();
       setPatientDetails(data);
-    } catch (error) {
+    } 
+    catch (error) {
       console.error('Error fetching patient details:', error);
     }
   };
 
-
   const fetchEncounters = async () => {
     try {
+
       const response = await fetch(`http://localhost:8085/ipd/get-doctor-encounter-by-patient-id?patientId=`+ patientId);
       if (!response.ok) {
         throw new Error('Failed to fetch encounters');
       }
       const data = await response.json();
-      setEncounters(data);
 
-    } catch (error) {
-      console.error('Error fetching encounters:', error);
-    }
-  };
+      const encountersData = await Promise.all(data.map(async (item: any) => {
 
-
-
-
-
-  if (!patientDetails) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <>
-      <Header />
-      <IonCard>
-        <IonCardHeader>
-          <IonCardSubtitle>Patient ID: {patientDetails.patientId}</IonCardSubtitle>
-          <IonCardTitle>{patientDetails.name}</IonCardTitle>
-        </IonCardHeader>
-        <IonCardContent>
-          <p>Age: {patientDetails.age}</p>
-          <p>Reason for Admit: {patientDetails.reasonForAdmit}</p>
-        </IonCardContent>
-      </IonCard>
-
-      {encounters.map((encounter, index) => (
-        <IonCard key={index}>
-          <IonCardHeader>
-            <IonCardTitle>Encounter {index + 1}</IonCardTitle>
-          </IonCardHeader>
-          <IonCardContent>
-            {/* <p>Notes: {encounter.patientId}</p>
-            <p>Nurse Instructions: {encounter.doctorId}</p>
-            <p>Medicines:{encounter.prescriptionId}</p> */}
-            <PrescriptionData prescriptionId={encounter.prescriptionId} />
-          </IonCardContent>
-        </IonCard>
-      ))}
-    </>
-  );
-};
-
-
-interface PrescriptionDataProps {
-  prescriptionId: any;
-}
-
-const PrescriptionData: React.FC<PrescriptionDataProps> = ({ prescriptionId }) => {
-  const [prescriptionData, setPrescriptionData] = useState<any | null>(null);
-  // const [medicationIds, setMedicationIds] = useState<any[]>([]);
-  const [medicationData, setMedicationData] = useState<any[]>([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const prescriptionResponse = await fetch(`http://localhost:8085/ipd/get-prescription?prescriptionId=${prescriptionId}`);
+        const prescriptionResponse = await fetch(`http://localhost:8085/ipd/get-prescription?prescriptionId=${item.prescriptionId}`);
         if (!prescriptionResponse.ok) {
           throw new Error('Failed to fetch prescription');
         }
         const prescriptionData = await prescriptionResponse.json();
-        setPrescriptionData(prescriptionData);
-  
-        const medicationDataResponse = await fetch(`http://localhost:8085/ipd/get-medication-by-prescription-id?prescriptionId=${prescriptionId}`);
-        if (!medicationDataResponse.ok) {
-          throw new Error('Failed to fetch medication IDs');
-        }
-        const medicationData = await medicationDataResponse.json();
-        setMedicationData(medicationData);
-  
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-  
-    fetchData();
-  }, [prescriptionId]);
 
-  if (!prescriptionData) {
-    return <p>Loading prescription data...</p>;
-  }
+        const medicationResponse = await fetch(`http://localhost:8085/ipd/get-medication-by-prescription-id?prescriptionId=${item.prescriptionId}`);
+        if (!medicationResponse.ok) {
+          throw new Error('Failed to fetch medication');
+        }
+        const medicationData = await medicationResponse.json();
+
+        return {
+          encounter: item,
+          prescription: prescriptionData,
+          medications: medicationData
+        }
+      }))
+
+      setEncounters(encountersData);
+
+      console.log(encountersData);
+    } 
+    catch (error) {
+      console.error('Error fetching encounters:', error);
+    }
+  };
 
   return (
-    <>
-      <h1>Advice</h1>
-      <p>Notes: {prescriptionData.notes}</p>
-      <DrawingImage imageData={prescriptionData.scribbleNotes}/>
-      <AudioPlayback audioData={prescriptionData.audioNotes}/>
+    <IonPage>
+      <Header/>
+      <IonContent>
 
-      <p>Instructions: {prescriptionData.instructions}</p>
-      <DrawingImage imageData={prescriptionData.scribbleInstructions}/>
-      <AudioPlayback audioData={prescriptionData.audioInstructions}/>
+        <div className="border-2 border-solid border-black mx-10 my-5 p-3">
+          <p>Patient ID: {patientDetails?.id}</p>
+          <p>Name: {patientDetails?.name}</p>
+          <p>Age: {patientDetails?.age}</p>
+          <p>Gender: {patientDetails?.gender}</p>
+          <p>Blood Group: {patientDetails?.bloodGroup}</p>
+        </div>
 
-      <br></br>
-      <h1> Medication </h1>
-      <ul>
-        {medicationData.map((medication) => (
-          <li key={medication.medicationId}>
-            <p>Name: {medication.medicineName}</p>
-            <p>Quantity: {medication.quantity}</p>
-            <p>Time: {medication.time}</p>
-            <p>Duration: {medication.duration}</p>
-            <br></br>
-          </li>
+        {encounters.map((encounter, index) => (
+          <div key={index} className="border-2 border-solid border-black mx-10 my-5 p-3 flex flex-col gap-4">
+            <h1 className="text-2xl font-semibold">Encounter {index + 1}</h1>
+            <p>Doctor Id: {encounter.encounter.doctorId}</p>
+
+            <p>Notes: {encounter.prescription?.notes}</p>
+            <DrawingImage imageData={encounter.prescription?.scribbleNotes}/>
+            <AudioPlayback audioData={encounter.prescription?.audioNotes}/>
+
+            <p>Instructions: {encounter.prescription?.instructions}</p>
+            <DrawingImage imageData={encounter.prescription?.scribbleInstructions}/>
+            <AudioPlayback audioData={encounter.prescription?.audioInstructions}/>
+
+            <h2 className="text-xl font-semibold">Medicines</h2>
+            <ul className="flex flex-col gap-4">
+              {encounter.medications.map((medication,index) => (
+                <li key={index}>
+                  <h3 className="text-lg font-semibold">Medicine {index + 1}</h3>
+                  <p>Name: {medication.medicineName}</p>
+                  <p>Quantity: {medication.quantity}</p>
+                  <p>Time: {medication.time}</p>
+                  <p>Duration: {medication.duration}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
         ))}
+      
+      
+        {/* <IonCard>
+          <IonCardHeader>
+            <IonCardSubtitle>Patient ID: {patientDetails?.id}</IonCardSubtitle>
+            <IonCardTitle>{patientDetails?.name}</IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent>
+            <p>Age: {patientDetails?.age}</p>
+            <p>Gender: {patientDetails?.gender}</p>
+            <p>Blood Group: {patientDetails?.bloodGroup}</p>
+          </IonCardContent>
+        </IonCard> */}
 
-      </ul>
-    </>
+        {/* {encounters.map((encounter, index) => (
+          <IonCard key={index}>
+            <IonCardHeader>
+              <IonCardTitle>Encounter {index + 1}</IonCardTitle>
+            </IonCardHeader>
+            <IonCardContent>
+              <p>Doctor Id: {encounter.encounter.doctorId}</p>
+      
+              <p>Notes: {encounter.prescription.notes}</p>
+              <DrawingImage imageData={encounter.prescription.scribbleNotes}/>
+              <AudioPlayback audioData={encounter.prescription.audioNotes}/>
+
+              <p>Instructions: {encounter.prescription.instructions}</p>
+              <DrawingImage imageData={encounter.prescription.scribbleInstructions}/>
+              <AudioPlayback audioData={encounter.prescription.audioInstructions}/>
+              
+              <ul>
+                {encounter.medications.map((medication) => (
+                  <li key={medication.medicationId}>
+                    <p>Name: {medication.medicineName}</p>
+                    <p>Quantity: {medication.quantity}</p>
+                    <p>Time: {medication.time}</p>
+                    <p>Duration: {medication.duration}</p>
+                    <br></br>
+                  </li>
+                ))}
+              </ul>
+            </IonCardContent>
+          </IonCard>
+        ))} */}
+
+      </IonContent>
+    </IonPage>
   );
 };
 
-export default PatientDetails;
+export default PatientDetailsPage;
